f58eaff5aba26d2efd6674545d62c0af76328830
camigonzalez568 <cg3857722@gmail.com>
2023-08-28 11:06:15 -1200

    ola

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('express')
+const app = ()
+const port = 3400
+
+app.listen(port ,()=>{
+    console.log('si anda eu')
+})
