7836c643226d2899e2f7373a5d364b0f8e6042a4
chtello <chtello@escuelasproa.edu.ar>
2023-08-30 16:47:52 -0300

    avances

---
+++ scrip.js
@@ -0,0 +1,4 @@
+function mostrarTexto() {
+    const texto = document.getElementById("texto");
+    texto.style.display = "block";
+}
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('express')
-const app = ()
-const port = 3400
+const express = require('express');
+const morgan = require('morgan');
+const path = require('express');
+const app = ('');
+const port = 3400;
 
 app.listen(port ,()=>{
     console.log('si anda eu')
